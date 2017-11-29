80bc04d08c30c6fffc96d3cb50fdc6d4c2c22a57
lenovo <lenovo@liyinhui>
2017-11-29 15:15:31 +0800

    11/29/15.15

---
+++ src/org/qhit/model/Emp.java
@@ -0,0 +1,19 @@
+package org.qhit.model;
+public class Emp {
+	private Integer eid;
+	private String ename;
+	public Integer getEid() {
+		return eid;
+	}
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+
+}
